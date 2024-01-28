7b0213376a511ec768ff18a1c75e6c307cc2f714
Mirac Gulgonul <mlg556@gmail.com>
2024-01-28 21:17:33 +0300

    15.1.1

--- src/vm.zig
+++ src/vm.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const z = @import("zlox.zig");
 
+pub const STACK_MAX = 256;
+
 pub const InterpretResult = enum(u8) { OK, COMPILE_ERROR, RUNTIME_ERROR };
 
 pub const VM = struct {
@@ -8,7 +10,30 @@ pub const VM = struct {
     /// Instruction Pointer: it's an index, unline the original implementation
     ip: usize = 0,
 
-    pub fn init() void {}
+    stack: [STACK_MAX]z.Value = undefined,
+
+    /// stack_top is also an index, because arrays and pointers are weird.
+    /// to get the element on top of the stack, you do stack[stack_top]
+    stack_top: usize = 0,
+
+    // stack stuff
+    fn reset_stack(vm: *VM) void {
+        vm.stack_top = 0;
+    }
+
+    pub fn push(vm: *VM, val: z.Value) void {
+        vm.stack[vm.stack_top] = val;
+        vm.stack_top += 1;
+    }
+
+    pub fn pop(vm: *VM) z.Value {
+        defer vm.stack_top -= 1;
+        return vm.stack[vm.stack_top];
+    }
+
+    pub fn init(vm: *VM) void {
+        vm.reset_stack();
+    }
 
     pub fn interpret(vm: *VM, chunk: *z.Chunk) InterpretResult {
         vm.chunk = chunk.*;
